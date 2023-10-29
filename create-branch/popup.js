12d9cfdb200d894ceb88cd4ec3fd5aec38ddfb96
hitnei <hoangnhi050298@gmail.com>
2023-10-29 17:57:53 +0700

    event listener input

--- create-branch/popup.js
+++ create-branch/popup.js
@@ -1,11 +1,16 @@
-import { getActiveTabUrl } from "./utils.js";
+import { getActiveTabUrl, getCBName } from "./utils.js";
 
-document.getElementById("CBName").addEventListener("change", async (e) => {
-  const CBName = e?.target?.value;
+document.getElementById("CBName").addEventListener("input", async (e) => {
+  const CBName = e?.target?.value?.trim();
   const activeTab = await getActiveTabUrl();
 
   chrome.tabs.sendMessage(activeTab.id, {
     type: "onChangeName",
     CBName,
   });
+});
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const CBName = await getCBName();
+  document.getElementById("CBName").value = CBName?.CBName || "";
 });
--- create-branch/utils.js
+++ create-branch/utils.js
@@ -4,3 +4,7 @@ export async function getActiveTabUrl() {
   let [tab] = await chrome.tabs.query(queryOptions);
   return tab;
 }
+
+export const setCBName = (value) => chrome.storage.sync?.set({ CBName: value });
+
+export const getCBName = () => chrome.storage.sync?.get("CBName");
